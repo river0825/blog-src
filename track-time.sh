45718d4e7f142bfa49a7d71bc06d156be7e0a9eb
river <river@pchomepay.com.tw>
2019-11-07 19:58:12 +0800

    change modify time

--- track-time.sh
+++ track-time.sh
@@ -6,5 +6,6 @@ do
     TIME=$(git log --pretty=format:%cd -n 1 --date=iso -- "$FILE")
     # echo $TIME $FILE
     TIME1=$(echo ${TIME//[-:+]/ } | awk -F ' ' '{print $1$2$3$4$5"."$6}')
-    echo $TIME1 $FILE 
+    echo $TIME $FILE
+    touch -m -t "${TIME1}" "$FILE"
 done
