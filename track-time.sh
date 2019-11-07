f93474f1340df3b3e2cc077c8d73295487ff88f2
river <river@pchomepay.com.tw>
2019-11-07 19:42:54 +0800

    fix track time

--- track-time.sh
+++ track-time.sh
@@ -4,7 +4,7 @@ IFS="
 for FILE in $(git ls-files)
 do
     TIME=$(git log --pretty=format:%cd -n 1 --date=iso -- "$FILE")
-    # TIME=$(date -j -f '%Y-%m-%d %H:%M:%S %z' "$TIME" +%Y%m%d%H%M.%S)
-    # echo $TIME $FILE 
-    touch -m -t "$TIME" "$FILE"
+    # echo $TIME $FILE
+    TIME1=$(echo ${TIME//[-:+]/ } | awk -F ' ' '{print $1$2$3$4$5"."$6}')
+    echo $TIME1 $FILE 
 done
